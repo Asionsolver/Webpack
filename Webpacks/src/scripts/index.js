1170d6f68bef770b36b05c0dc7e8a89443b1d0de
Asionsolver <asionsolver274@gmail.com>
2022-06-16 10:43:21 +0600

    add New folder Webpack

---
+++ Webpacks/src/scripts/index.js
@@ -0,0 +1,8 @@
+import {abc} from './test';
+
+const fn = () => {
+    console.log('Hello World');
+}
+
+fn();
+console.log(abc);
---
+++ Webpacks/webpack.config.js
@@ -0,0 +1,13 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/scripts/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    mode: 'development' 
+
+}
+
+// index.js file entry file
